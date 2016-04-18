1f12d270b55f97a32fe7c7f8d679c9ed5cc5b846
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-17 19:30:22 -0500

    Se agrega model usuarioRol

---
+++ app/models/usuarioRol.server.model.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UsuarioRolSchema = new Schema({
+    ur_rol: {type: Schema.ObjectId, ref: 'Rol'},
+    ur_user: {type: Schema.ObjectId, ref: 'User'},
+    ur_status: {type: String, default: 'V'}
+});
+
+UsuarioRolSchema.index({ur_user: 1, ur_rol: 1}, {unique: true});
+mongoose.model('UsuarioRol', UsuarioRolSchema);
