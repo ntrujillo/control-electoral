e93c72e53e20ddddfd0a8944614bf120059a89c6
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-18 22:27:54 -0500

    Actualización de rutas

--- app/routes/usuarioRol.server.route.js
+++ app/routes/usuarioRol.server.route.js
@@ -1,6 +1,9 @@
-var usuarioRol = require('../../app/controllers/usuarioRol.server.controller');
+var usuarioRol = require('../../app/controllers/usuarioRol.server.controller'),
+    usuario = require('../../app/controllers/users.server.controller');
 
 module.exports = function (app) {
     app.route('/api/usuarioRol')
         .post(usuarioRol.saveUsuarioRol);
+    app.route('/api/usuarioRol/:idUsuario')
+        .get(usuario.requiresLogin, usuarioRol.getRolByUsuario);
 };
