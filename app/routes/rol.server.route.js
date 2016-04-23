b6ab2db9b7422083b41f8608634f68b7aaff9e37
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-23 17:55:30 -0500

    Actualización de controlador

--- app/routes/rol.server.route.js
+++ app/routes/rol.server.route.js
@@ -1,6 +1,9 @@
-var rol = require('../../app/controllers/rol.server.controller');
+var rol = require('../../app/controllers/rol.server.controller'),
+    user = require('../../app/controllers/users.server.controller');
 
 module.exports = function (app) {
     app.route('/api/rol')
         .post(rol.saveRol);
+    app.route('/api/rol/:rol')
+        .get(user.requiresLogin, rol.getStatusRol);
 };
