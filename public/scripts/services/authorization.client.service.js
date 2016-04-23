96eeefb9b1e7d94cb69a03bf1b4f9aaee090373a
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-23 17:57:52 -0500

    Actualización de servicio

--- public/scripts/services/authorization.client.service.js
+++ public/scripts/services/authorization.client.service.js
@@ -1,6 +1,9 @@
 (function (angular) {
     angular.module('ControlElectoralApp').factory('Authorization', ['$resource', function ($resource) {
-        return $resource('/api/usuarioRol/:idUsuario', {query: {method: "GET", isArray: true}});
+        var service = {};
+        service.rolesByUser = $resource('/api/usuarioRol/:idUsuario', {query: {method: "GET", isArray: true}});
+        service.rolStatus = $resource('/api/rol/:rol');
+        return service;
     }]);
 
 }(window.angular));
