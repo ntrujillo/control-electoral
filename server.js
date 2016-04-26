3158c0b92f2cdb2cb2fc2eb5f4f733a4e061811c
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-25 22:14:08 -0500

    Se actualiza server

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose');
 var passport = require('./config/passport');
 var express = require('./config/express');
+var sequelize = require('./config/sequelize-connect');
+var Logger = require('./app/log/ILogger');
 
-var port = 3006;
+var loggerApi = new Logger('Server');
+var port = 3002;
 var db = mongoose();
+//en caso se necesite cambiar a mysqlPostgres
+//var dbSe= sequelize();
 var app = express();
 //var app = express(db);
 var passport = passport();
 
-
-
 app.listen(port);
 module.exports = app;
-
-console.log('Server running at http://localhost:' + port);
+loggerApi.info('Server running at http://localhost:', port);
