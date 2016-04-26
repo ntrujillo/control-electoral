f0a507e7b9aa7349a42130ec2340b8ad20f408e9
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-04-25 22:09:23 -0500

    Se agrega archivo Logger

---
+++ app/log/Logger.js
@@ -0,0 +1,55 @@
+var winston = require('winston'),
+    path = require('path');
+
+winston.emitErrs = true;
+var directory = __dirname + '/../../logs';
+
+var constants = {
+    LEVEL_LOG: {
+        ERROR: 'error',
+        WARN: 'warn',
+        INFO: 'info',
+        VERBOSE: 'verbose',
+        DEBUG: 'debug',
+        SILLY: 'silly'
+    },
+    FILE: {
+        FILE_NAME: 'app-log.log',
+        FILE_EXCEPTION: 'exceptions.log'
+    }
+};
+
+var fileNameLog = path.join(directory, constants.FILE.FILE_NAME);
+var fileNameExceptionLog = path.join(directory, constants.FILE.FILE_EXCEPTION);
+
+var logger = new (winston.Logger)({
+    transports: [
+        new winston.transports.File({
+            level: constants.LEVEL_LOG.INFO,
+            filename: fileNameLog,
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: true
+        }),
+        new winston.transports.Console({
+            level: constants.LEVEL_LOG.DEBUG,
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({filename: fileNameExceptionLog})
+    ],
+    exitOnError: false
+});
+
+module.exports = constants;
+module.exports.stream = {
+    write: function (message) {
+        logger.info(message);
+    }
+};
+module.exports = logger;
