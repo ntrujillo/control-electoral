17b7e6ce44da0a8403acde4c0d0d6c2d43a6b39f
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-02 00:54:53 -0500

    actualización

--- public/scripts/controllers/admin/user.client.controller.js
+++ public/scripts/controllers/admin/user.client.controller.js
@@ -17,13 +17,24 @@
             }
 
             getUsers();
-            function showModal() {
+            function showModal(idUSer) {
+                var template;
+                if (angular.isDefined(idUSer)) {
+                    template = 'views/admin/user-modal-edit.html';
+                } else {
+                    template = 'views/admin/user-modal.html';
+                }
                 var modalInstance = $modal.open({
-                    templateUrl: 'views/admin/user-modal.html',
+                    templateUrl: template,
                     controller: 'UserModalCtrl as ctrl',
                     backdrop: 'static',
                     animation: true,
                     resolve: {
+                        userModel: function () {
+                            return {
+                                idUser: idUSer
+                            };
+                        },
                         deps: ['$ocLazyLoad', function ($ocLazyLoad) {
                             return $ocLazyLoad.load({
                                     name: 'angular-factory',
@@ -36,10 +47,11 @@
                     }
                 });
 
-                modalInstance.result.then(function (obj) {
-                    ctrl.result = obj;
+                modalInstance.result.then(function () {
+                    getUsers();
                 });
-            };
+            }
+
             ctrl.showModal = showModal;
         }
 
