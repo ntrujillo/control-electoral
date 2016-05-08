00f4e0b050d90eebf81b2438be4b9d44e475f28f
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-07 23:08:46 -0500

    Se actualiza

--- public/scripts/services/admin/user.client.service.js
+++ public/scripts/services/admin/user.client.service.js
@@ -2,6 +2,7 @@
     angular.module('ControlElectoralApp').factory('Users', ['$resource', function ($resource) {
         var service = {};
         service = $resource('/api/users/:idUser', {idUser: '@_id'}, {update: {method: 'PUT'}});
+        service.Api = $resource('/api/user/:cedula');
         return service;
     }]);
 
