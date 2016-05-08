ea3c3ea20dc78a77c8b6cb9c34aafcf55a93174b
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-08 01:05:35 -0500

    Se actualiza

--- app/controllers/rol.server.controller.js
+++ app/controllers/rol.server.controller.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose'),
+    Logger = require(__dirname + '/../../app/log/Logger'),
     Rol = mongoose.model('Rol');
 
 var getErrorMessage = function (err) {
     if (err) {
         for (var errName in err.errors) {
-            if (err.errors[errName].message) {
+            if (err.errors.hasOwnProperty(errName)) {
                 return err.errors[errName].message;
             }
         }
@@ -38,5 +39,28 @@ exports.getStatusRol = function (req, res) {
                 res.status(200).json({'status': false, 'message': 'CONTAINER.SECURITY.LBL_ROL_INVALID'});
             }
         }
+    });
+};
+
+exports.getRoles = function (req, res) {
+    var pagina, registros;
+    pagina = req.query.page;
+    registros = req.query.numRegistros;
+
+    Rol.find({}).count(function (err, count) {
+        if (err) {
+            Logger.logError('[RolCtrl] Error al obtener el número de roles', err);
+            res.status(500);
+        }
+        res.header('X-Total-Count', count);
+        Rol.find({}, '-ro_creator').sort({ro_description: 1}).limit(parseInt(registros)).skip(parseInt(registros) * (parseInt(pagina) - 1)).exec(function (err, roles) {
+            if (err) {
+                Logger.logError('[RolCtrl] Falla de infraestructura', getErrorMessage(err));
+                return res.status(400).json(getErrorMessage(err));
+            } else {
+                Logger.logInfo('[RolCtrl] Roles obtenidos', JSON.stringify(roles));
+                return res.status(200).json(roles);
+            }
+        });
     });
 };
