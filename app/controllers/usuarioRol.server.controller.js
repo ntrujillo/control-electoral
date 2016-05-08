8a3b2b3a32842623bbf257cf14440d950a811ae2
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-07 22:32:53 -0500

    Se actualiza

--- app/controllers/usuarioRol.server.controller.js
+++ app/controllers/usuarioRol.server.controller.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose'),
+    Logger = require(__dirname + '/../../app/log/Logger'),
     UsuarioRol = mongoose.model('UsuarioRol');
 
 var getErrorMessage = function (err) {
     if (err) {
         for (var errName in err.errors) {
-            if (err.errors[errName].message) {
+            if (err.errors.hasOwnProperty(errName)) {
                 return err.errors[errName].message;
             }
         }
@@ -28,8 +29,10 @@ exports.getRolByUsuario = function (req, res) {
     var idUsuario = req.params.idUsuario;
     UsuarioRol.find().where('ur_user').equals(idUsuario).populate('ur_rol', 'ro_rol ro_description ro_creator ro_status').exec(function (err, roles) {
         if (err) {
+            Logger.logError('[UsuarioRol] Error al obtener lor roles para el usuario' + idUsuario, getErrorMessage(err));
             return res.status(400).send({message: getErrorMessage(err)});
         } else {
+            Logger.logInfo('[UsuarioRol] Roles para el usuario ' + idUsuario, roles);
             res.status(200).json(roles);
         }
     });
