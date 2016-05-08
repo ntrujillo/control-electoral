3cca8eb22508a3fc3d55e9b27f0e1a9f45343d58
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-08 01:03:58 -0500

    Se actualiza

--- app/routes/rol.server.route.js
+++ app/routes/rol.server.route.js
@@ -3,6 +3,7 @@ var rol = require('../../app/controllers/rol.server.controller'),
 
 module.exports = function (app) {
     app.route('/api/rol')
+        .get(user.requiresLogin, rol.getRoles)
         .post(rol.saveRol);
     app.route('/api/rol/:rol')
         .get(user.requiresLogin, rol.getStatusRol);
