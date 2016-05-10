c7642a0b7b295793264d6b659a5b4e7461fbf718
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-09 21:17:45 -0500

    Se actualiza

--- app/controllers/usuarioRol.server.controller.js
+++ app/controllers/usuarioRol.server.controller.js
@@ -16,10 +16,13 @@ var getErrorMessage = function (err) {
 
 exports.saveUsuarioRol = function (req, res) {
     var rolUsuario = new UsuarioRol(req.body);
+    Logger.logError('[UsuarioRolCtrl]Rolll', rolUsuario);
     rolUsuario.save(function (err) {
         if (err) {
+            Logger.logError('[UsuarioRolCtrl] Error al guardar el rol', getErrorMessage(err));
             return res.status(400).send({message: getErrorMessage(err)});
         } else {
+            Logger.logInfo('[UsuarioRolCtrl] Rol guardado');
             res.status(200).json(rolUsuario);
         }
     });
@@ -29,11 +32,19 @@ exports.getRolByUsuario = function (req, res) {
     var idUsuario = req.params.idUsuario;
     UsuarioRol.find().where('ur_user').equals(idUsuario).populate('ur_rol', 'ro_rol ro_description ro_creator ro_status').exec(function (err, roles) {
         if (err) {
-            Logger.logError('[UsuarioRol] Error al obtener lor roles para el usuario' + idUsuario, getErrorMessage(err));
+            Logger.logError('[UsuarioRolCtrl] Error al obtener lor roles para el usuario' + idUsuario, getErrorMessage(err));
             return res.status(400).send({message: getErrorMessage(err)});
         } else {
-            Logger.logInfo('[UsuarioRol] Roles para el usuario ' + idUsuario, roles);
-            res.status(200).json(roles);
+            var rolesArray = [];
+            if (roles instanceof Array) {
+                roles.forEach(function (element) {
+                    if (element.ur_rol.ro_status === 'V') {
+                        rolesArray.push(element);
+                    }
+                });
+            }
+            Logger.logInfo('[UsuarioRolCtrl] Roles para el usuario ' + idUsuario, rolesArray);
+            res.status(200).json(rolesArray);
         }
     });
 };
