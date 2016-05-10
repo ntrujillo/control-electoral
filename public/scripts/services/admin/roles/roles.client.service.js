cb5c23d282cc976baaec4796875587a9f679b384
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-09 21:12:14 -0500

    Se actualiza

--- public/scripts/services/admin/roles/roles.client.service.js
+++ public/scripts/services/admin/roles/roles.client.service.js
@@ -1,6 +1,11 @@
 (function (angular) {
     angular.module('ControlElectoralApp').factory('RolesSrv', ['$resource', function ($resource) {
-        return $resource('/api/rol');
+        var service = {};
+        service = $resource('/api/rol');
+        service.Api = $resource('/api/rolId/:_idRol', {_idRol: '@_id'}, {update: {method: 'PUT'}});
+        service.Roles = $resource('/api/roles');
+        service.RolesByUSer = $resource('/api/usuarioRol');
+        return service;
     }]);
 
 }(window.angular));
