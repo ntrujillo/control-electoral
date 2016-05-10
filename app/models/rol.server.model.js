179fd42da1db08c11f6579f3143b65578e58e3df
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-09 21:17:21 -0500

    Se actualiza

--- app/models/rol.server.model.js
+++ app/models/rol.server.model.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var RolSchema = new Schema({
-    ro_rol: {type: Number, unique: true, required: 'Rol is required'},
+    ro_rol: {type: Number, unique: true},
     ro_description: {type: String, trim: true},
     ro_created: {type: Date, default: Date.now},
     ro_creator: {type: Schema.ObjectId, ref: 'User'},
-    ro_status: {type: String, default: 'V', required: 'Status is required'}
+    ro_status: {type: String, default: 'V'}
 });
 
 mongoose.model('Rol', RolSchema);
