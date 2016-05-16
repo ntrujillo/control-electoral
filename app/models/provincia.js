d4082875c49ba74958c5a5ab0aff128c0935e227
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-15 20:52:42 -0500

    Se actualiza

---
+++ app/models/provincia.js
@@ -0,0 +1,20 @@
+//mongoose es una libreria que me permite conectarme a MongoDB pero existen otras
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var Provincia = new Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    code: {
+        type: String,
+        unique: true,
+        trim: true
+    },
+    cantones: [{type: mongoose.Schema.Types.ObjectId, ref: 'Canton'}]
+});
+
+Provincia.index({code: 1});
+
+module.exports = mongoose.model('Provincia', Provincia);
