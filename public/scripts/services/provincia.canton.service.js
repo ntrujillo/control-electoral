5d8455af508a450546a67f77b98c7bcbb4a56666
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-16 01:29:23 -0500

    Se actualiza

---
+++ public/scripts/services/provincia.canton.service.js
@@ -0,0 +1,17 @@
+(function (angular) {
+
+    angular.module('ControlElectoralApp')
+        .factory('ProvinciaCantonResource', function ($resource) {
+            return $resource('/api/provincia/:id_provincia/canton/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
