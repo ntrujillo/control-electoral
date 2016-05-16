657c4d4320e8d9b0fb9c060a7d0aaaab7fbd5e6d
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-15 20:45:21 -0500

    Se actualiza

---
+++ public/scripts/services/provincia.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('ProvinciaResource', function ($resource) {
+            return $resource('/api/provincia/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
