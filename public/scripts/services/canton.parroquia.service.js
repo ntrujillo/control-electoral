c649c13abe8df8e7347fbf451b73c582f30a5b0f
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-16 22:22:12 -0500

    Se actualiza

---
+++ public/scripts/services/canton.parroquia.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('CantonParroquiaResource', function ($resource) {
+            return $resource('/api/canton/:id_canton/parroquia/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
