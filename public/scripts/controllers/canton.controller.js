42bdcb06b531002cf7c7b7460badccdda142949b
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-16 21:56:38 -0500

    Se actualiza

---
+++ public/scripts/controllers/canton.controller.js
@@ -0,0 +1,33 @@
+(function (angular) {
+
+    angular.module('ControlElectoralApp')
+        .controller('CantonCtrl', ['CantonResource', 'APP', '$scope', function (CantonResource, constant, $scope) {
+            var ctrl = this;
+            ctrl.registros = [];
+            ctrl.pageno = 1;
+            ctrl.total_count = 0;
+            ctrl.itemsPerPage = 5;
+
+            function loadData(page) {
+                CantonResource.query({
+                    page: page,
+                    per_page: ctrl.itemsPerPage,
+                    q: ctrl.code
+                }, function (result, headers) {
+                    ctrl.registros = result;
+                    ctrl.total_count = headers('X-Total-Count');
+                }, function (errorResponse) {
+                    $scope.notification.showErrorWithFilter(errorResponse.data.message, constant.COMMONS.ERROR);
+                });
+            }
+
+
+            function refresh() {
+                loadData(ctrl.pageno);
+            }
+
+            ctrl.refresh = refresh;
+            ctrl.loadData = loadData;
+            ctrl.refresh();
+        }]);
+}(window.angular));
