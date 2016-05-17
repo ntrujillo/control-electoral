0b6f22a288c86db8eaa81cd1bae9f9756ed3b78e
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-17 02:30:39 -0500

    Se actualiza

--- config/express.js
+++ config/express.js
@@ -44,7 +44,7 @@ module.exports = function () {
     require('../app/routes/registro-route.js')(app);
     require('../app/routes/detail-route.js')(app);
     require('../app/routes/provincia.server.route.js')(app);
-    require('../app/routes/canton.server.route.js')(app);
+    require('../app/routes/canton.route.js')(app);
     require('../app/routes/parroquia.server.route.js')(app);
     require('../app/routes/zona.server.route.js')(app);
     require('../app/routes/recinto.server.route.js')(app);
