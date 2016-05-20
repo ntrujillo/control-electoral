7495855c0f624a9bdacd601cb4581f93bc8d8a01
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-20 01:01:24 -0500

    Se actualiza

---
+++ public/scripts/services/parroquia.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('ParroquiaResource', function ($resource) {
+            return $resource('/api/parroquia/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
