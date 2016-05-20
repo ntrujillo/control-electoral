e6157943f8164e7b8a3efce41c3c9f6bd972d584
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-20 00:19:45 -0500

    Se actualiza

---
+++ public/scripts/controllers/parroquia-dialog.controller.js
@@ -0,0 +1,45 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('ControlElectoralApp').controller('ParroquiaDialogCtrl',
+        ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'CantonParroquiaResource', 'Notification',
+            function ($scope, $stateParams, $modalInstance, entity, CantonParroquiaResource, Notification) {
+                $scope.initData = function (id) {
+                    CantonParroquiaResource.get({id_canton: $stateParams.id, id: id}, function (result) {
+                        $scope.parroquia = result;
+                    });
+                };
+                var onSaveFinished = function (result) {
+                    $modalInstance.close(result);
+                };
+                $scope.save = function () {
+                    if ($scope.parroquia._id != null) {
+                        CantonParroquiaResource.update({
+                            id_canton: $stateParams.id,
+                            id: $scope.parroquia._id
+                        }, $scope.parroquia, function (response) {
+                            Notification.success(response.message, 'CONTAINER.MESSAGES.MESSAGE_SUCCESS', 5000);
+                            onSaveFinished();
+                        });
+                    } else {
+                        CantonParroquiaResource.save({id_canton: $stateParams.id}, $scope.parroquia, function (response) {
+                            Notification.success(response.message, 'CONTAINER.MESSAGES.MESSAGE_SUCCESS', 2000);
+                            onSaveFinished();
+                        }, function (errorResponse) {
+                            if (angular.isDefined(errorResponse.data.message)) {
+                                Notification.showErrorWithFilter(errorResponse.data.message, 'CONTAINER.MESSAGES.MESSAGE_ERROR');
+                            } else {
+                                Notification.showErrorWithFilter(errorResponse.data, 'CONTAINER.MESSAGES.MESSAGE_ERROR');
+                            }
+                        });
+                    }
+                };
+                $scope.clear = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+                if (entity) {
+                    $scope.initData(entity._id);
+                }
+
+            }]);
+}(window.angular));
