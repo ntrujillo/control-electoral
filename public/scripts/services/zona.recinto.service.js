0b15674be13620ec8d4264b91968ae2aa171ba7d
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-21 18:25:46 -0500

    Se actualiza

---
+++ public/scripts/services/zona.recinto.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('ZonaRecintoResource', function ($resource) {
+            return $resource('/api/zona/:id_zona/recinto/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
