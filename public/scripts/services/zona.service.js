4ae368971da1595a39e0e20e53514a399c3ffeec
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-21 15:35:26 -0500

    Se actualiza

---
+++ public/scripts/services/zona.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('ZonaResource', function ($resource) {
+            return $resource('/api/zona/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
