6e2e7c3c59c26994302743667899d32d1a5d567d
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-22 13:14:52 -0500

    Se actualiza

---
+++ app/models/junta.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var JuntaSchema = new Schema({
+    gender: {
+        type: String,
+        trim: true
+    },
+    junta: {
+        type: String,
+        trim: true
+    },
+    empadronados: {
+        type: Number,
+        trim: true
+    },
+    recinto: {type: mongoose.Schema.Types.ObjectId, ref: 'Recinto'}
+
+});
+JuntaSchema.index({junta: 1, gender: 1, recinto: 1}, {unique: true});
+mongoose.model('Junta', JuntaSchema);
