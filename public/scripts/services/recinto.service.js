5b684a7dcfec1aebc463208b3f2a1a103cd4f95f
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-21 22:36:36 -0500

    Se actualiza

---
+++ public/scripts/services/recinto.service.js
@@ -0,0 +1,16 @@
+(function (angular) {
+    angular.module('ControlElectoralApp')
+        .factory('RecintoResource', function ($resource) {
+            return $resource('/api/recinto/:id', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
+}(window.angular));
