fd32f9bee72d3a682d33b85645ef082c4b7ef952
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-22 22:43:06 -0500

    Se actualiza

--- app/routes/lista.server.route.js
+++ app/routes/lista.server.route.js
@@ -1,8 +1,13 @@
-var lista = require('../../app/controllers/lista.server.controller');
+var usuario = require('../../app/controllers/users.server.controller'),
+    lista = require('../../app/controllers/lista.server.controller');
 
 module.exports = function (app) {
-    app.route('/api/lista')
-        .get(lista.getListas).
-        post(lista.saveLista);
+    app.route('/api/lista/')
+        .get(usuario.requiresLogin, lista.getListas).
+        post(usuario.requiresLogin, lista.saveLista);
+    app.route('/api/lista/:id_lista')
+        .get(usuario.requiresLogin, lista.getListaById)
+        .put(usuario.requiresLogin, lista.updateLista)
+        .delete(usuario.requiresLogin, lista.deleteLista);
 
 };
