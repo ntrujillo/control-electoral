c0c57a879b91058691724744067d8a17775b9afc
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-22 22:39:29 -0500

    Se actualiza

--- public/scripts/services/lista.client.service.js
+++ public/scripts/services/lista.client.service.js
@@ -1,7 +1,16 @@
 (function (angular) {
-    'use strict';
-    angular.module('ControlElectoralApp').factory('Lista', ['$resource', function ($resource) {
-        return $resource('/api/lista', {query: {method: "GET", isArray: true}});
-    }]);
-
+    angular.module('ControlElectoralApp')
+        .factory('Lista', function ($resource) {
+            return $resource('/api/lista/:id_lista', {}, {
+                'query': {method: 'GET', isArray: true},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        data = angular.fromJson(data);
+                        return data;
+                    }
+                },
+                'update': {method: 'PUT'}
+            });
+        });
 }(window.angular));
