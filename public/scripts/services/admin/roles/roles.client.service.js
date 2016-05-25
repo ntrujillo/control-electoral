cdd85b12452d35f367cdccd958847758d9e4e938
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-25 00:06:22 -0500

    Se actualiza

--- public/scripts/services/admin/roles/roles.client.service.js
+++ public/scripts/services/admin/roles/roles.client.service.js
@@ -4,7 +4,7 @@
         service = $resource('/api/rol');
         service.Api = $resource('/api/rolId/:_idRol', {_idRol: '@_id'}, {update: {method: 'PUT'}});
         service.Roles = $resource('/api/roles');
-        service.RolesByUSer = $resource('/api/usuarioRol');
+        service.RolesByUSer = $resource('/api/usuarioRol/:idUsuario/:idRol');
         return service;
     }]);
 
