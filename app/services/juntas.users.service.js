3b24d3e5b1eedee8bdd628f11020e6d86f8a1911
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-05-26 23:44:14 -0500

    Se actualiza

--- app/services/juntas.users.service.js
+++ app/services/juntas.users.service.js
@@ -16,16 +16,12 @@ function create(id_user, body) {
             response.junta.push(body.junta);
 
         } else {
-            response.id_user = id_user;
-            response.junta = body.junta;
+            response = new JuntaUser({id_user: id_user, junta: body.junta});
         }
         response.save(function (err) {
             if (err)deferred.reject(err);
             Junta.findOne({_id: body.junta}, function (err, jun) {
                 if (err) deferred.reject(err);
-                jun.junta = body.junta;
-                jun.gender = body.gender;
-                jun.empadronados = body.empadronados;
                 jun.status = body.status;
                 jun.save(function (err) {
                     if (err)deferred.reject(err);
