177023fa8817e331b5d73dcbb641420d163407b2
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-01 00:56:50 -0500

    Se actualiza

--- app/routes/junta.user.route.js
+++ app/routes/junta.user.route.js
@@ -4,6 +4,7 @@ var juntaUser = require('../../app/controllers/juntas.users.controller'),
 module.exports = function (app) {
 
     app.route('/api/juntasUser/:id_user')
-        .post(usuario.requiresLogin, juntaUser.createJuntaUser);
+        .post(usuario.requiresLogin, juntaUser.createJuntaUser)
+        .get(usuario.requiresLogin, juntaUser.getJuntaUser);
 
 };
