111b8e82c5d00ace9240a4eac5a0400aa5151b7c
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-01 00:57:02 -0500

    Se actualiza

--- app/controllers/juntas.users.controller.js
+++ app/controllers/juntas.users.controller.js
@@ -12,4 +12,26 @@ function createJuntaUser(req, res) {
             return res.status(400).send({message: err});
         });
 }
+function getJuntaUser(req, res) {
+    var fields = req.query.fields;
+    var sort = req.query.sort;
+    var page = req.query.page;
+    var perPage = req.query.per_page;
+
+    service.query(req.params.id_user, fields, sort, page, perPage)
+        .then(function (response) {
+            if (response.juntasUser) {
+                Logger.logInfo('[JuntaUserCtrl] Se recuper\u00f3 las juntas asignadas al usuario', req.params.id_user);
+                res.header('X-Total-Count', response.count);
+                res.send(response.juntasUser);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            Logger.logError('[JuntaUserCtrl] Error al obtener las juntas asignadas al usuario', req.params.id_user);
+            res.status(400).send(err);
+        });
+}
 module.exports.createJuntaUser = createJuntaUser;
+module.exports.getJuntaUser = getJuntaUser;
