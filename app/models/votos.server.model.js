b7a5984a1a7a2613958a4c90552ace7e41a46937
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-02 01:55:20 -0500

    Se actualiza

--- app/models/votos.server.model.js
+++ app/models/votos.server.model.js
@@ -19,49 +19,11 @@ var VotoSchema = new Schema({
         LISTA: {type: Schema.ObjectId, ref: 'Lista'}
     }],
     JUNTA: {
-        _id: String,
-        NUM_JUNTA: Number,
-        GENERO: String,
-        NUM_EMPADR: Number,
-        RECINTO: {
-            _id: String,
-            COD_RECINTO: Number,
-            NOM_RECINTO: String,
-            DIR_RECINTO: String,
-            TEL_RECINTO: String,
-            JUN_FEM: Number,
-            JUN_MAS: Number,
-            NUM_JUNR: Number,
-            JUN_INIM: Number,
-            JUN_FINM: Number,
-            JUN_INIF: Number,
-            JUN_FINF: Number,
-            STATUS: String,
-            ACTAS: String,
-            RESTO: String,
-            LAT_RECINTO: Number,
-            LONG_RECINTO: Number,
-            ZONA: {
-                _id: String,
-                COD_ZONA: Number,
-                NOM_ZONA: String,
-                PARROQUIA: {
-                    _id: String,
-                    COD_PARROQUIA: Number,
-                    NOM_PARROQUIA: String,
-                    CANTON: {
-                        _id: String,
-                        COD_CANTON: Number,
-                        NOM_CANTON: String,
-                        PROVINCE: {
-                            _id: String,
-                            COD_PROVINCIA: Number,
-                            NOM_PROVINCIA: String
-                        }
-                    }
-                }
-            }
-        }
+        type: mongoose.Schema.Types.ObjectId, ref: 'Junta'
+    },
+    FECHA_REGISTRO: {
+        type: Date,
+        default: Date.now
     }
 });
 mongoose.model('Voto', VotoSchema);
