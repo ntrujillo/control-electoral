a03ae35f29b5ff136ee32a21e3ddcb6a608098e0
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-05 01:39:13 -0500

    Se actualiza

--- public/scripts/services/filtro-busqueda.client.service.js
+++ public/scripts/services/filtro-busqueda.client.service.js
@@ -2,9 +2,9 @@
     'use strict';
     angular.module('ControlElectoralApp').factory('Filtros', ['$resource', function ($resource) {
         var service = {};
-        service.Provincias = $resource('/api/provinces', {query: {method: "GET", isArray: true}});
-        service.Canton = $resource('/api/cantones/:codeProvince', {query: {method: "GET", isArray: true}});
-        service.Parroquia = $resource('/api/parroquias/:codeCanton', {query: {method: "GET", isArray: true}});
+        service.Provincias = $resource('/api/provincia/:id', {query: {method: "GET", isArray: true}});
+        service.Canton = $resource('/api/provincia/:id_provincia/canton', {query: {method: "GET", isArray: true}});
+        service.Parroquia = $resource('/api/canton/:id_canton/parroquia', {query: {method: "GET", isArray: true}});
         return service;
     }]);
 
