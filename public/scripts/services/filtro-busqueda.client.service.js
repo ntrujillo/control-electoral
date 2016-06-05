8258da07a3c819de6e017afe161c2c60e5b984a9
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-05 03:23:39 -0500

    Se actualiza

--- public/scripts/services/filtro-busqueda.client.service.js
+++ public/scripts/services/filtro-busqueda.client.service.js
@@ -5,6 +5,9 @@
         service.Provincias = $resource('/api/provincia/:id', {query: {method: "GET", isArray: true}});
         service.Canton = $resource('/api/provincia/:id_provincia/canton', {query: {method: "GET", isArray: true}});
         service.Parroquia = $resource('/api/canton/:id_canton/parroquia', {query: {method: "GET", isArray: true}});
+        service.Zona = $resource('/api/parroquia/:id_parroquia/zona/:id', {query: {method: "GET", isArray: true}});
+        service.Recinto = $resource('/api/zona/:id_zona/recinto/:id', {query: {method: "GET", isArray: true}});
+        service.Junta = $resource('/api/recinto/:id_recinto/junta', {query: {method: "GET", isArray: true}});
         return service;
     }]);
 
