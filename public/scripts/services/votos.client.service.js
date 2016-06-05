ec8c9b71dbed78f88770ae0ea23841db65530683
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-05 16:27:21 -0500

    Se actualiza

--- public/scripts/services/votos.client.service.js
+++ public/scripts/services/votos.client.service.js
@@ -8,21 +8,25 @@
         service.votosBlancoByProvince = $resource('/api/votos/votosBlancosProvincia/:codeProvince');
         service.votosBlancoByCanton = $resource('/api/votos/votosBlancosCanton/:codeCanton');
         service.votosBlancoByParroquia = $resource('/api/votos/votosBlancosParroquia/:codeParroquia');
+        service.votosBlancoFiltro = $resource('/api/votos/votosBlancosFiltro');
 
         service.votosNulosTotal = $resource('/api/votos/votosNulos');
         service.votosNulosByProvince = $resource('/api/votos/votosNulosProvincia/:codeProvince');
         service.votosNulosByCanton = $resource('/api/votos/votosNulosCanton/:codeCanton');
         service.votosNulosByParroquia = $resource('/api/votos/votosNulosParroquia/:codeParroquia');
+        service.votosNulosFiltro = $resource('/api/votos/votosNulosFiltro');
 
         service.votosTotales = $resource('/api/votos/votosTotales');
         service.totalVotosByProvince = $resource('/api/votos/votosTotalesProvincia/:codeProvince');
         service.totalVotosByCanton = $resource('/api/votos/votosTotalesCanton/:codeCanton');
         service.totalVotosByParroquia = $resource('/api/votos/votosTotalesParroquia/:codeParroquia');
+        service.votosTotalesFiltro = $resource('/api/votos/votosTotalesFiltro');
 
         service.totalVotosLista = $resource('/api/votos/votosLista/:codeLista');
         service.totalVotosListaProvincia = $resource('/api/votos/votosListaProvince/:codeProvince/:codeLista');
         service.totalVotosListaCanton = $resource('/api/votos/votosListaCanton/:codeCanton/:codeLista');
         service.totalVotosListaParroquia = $resource('/api/votos/votosListaParroquia/:codeParroquia/:codeLista');
+        service.totalVotosListaFiltro = $resource('/api/votos/totalVotosListaFiltro');
 
 
         return service;
