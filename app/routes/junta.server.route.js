9194292f6b91bee2588908a54967e2a9575435b7
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-06-11 23:32:09 -0500

    Se actualiza

--- app/routes/junta.server.route.js
+++ app/routes/junta.server.route.js
@@ -1,16 +1,14 @@
-var junta = require('../../app/controllers/junta.server.controller');
+var junta = require('../../app/controllers/junta.server.controller'),
+    usuario = require('../../app/controllers/users.server.controller');
 
 module.exports = function (app) {
-    /*app.route('/api/juntas/:codeRecinto')
-     .get(junta.getJuntasByRecinto);*/
-
     app.route('/api/juntas/:codeRecinto/:genero')
-        .get(junta.getJuntasByRecintoByGenero);
-
-    /*app.route('/api/juntas/:codeRecinto/:genero/:codeJunta')
-     .get(junta.getJunta);*/
+        .get(usuario.requiresLogin, junta.getJuntasByRecintoByGenero);
 
     app.route('/api/juntas/:id')
-        .get(junta.getJunta);
+        .get(usuario.requiresLogin, junta.getJunta);
+
+    app.route('/api/juntasRest/')
+        .get(usuario.requiresLogin, junta.getNumeroDeVotantes);
 
 };
