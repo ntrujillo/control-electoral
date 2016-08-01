62a8425ee1f176d896e74bee83f970db675b28ae
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-08-01 01:22:08 -0500

    actualización

--- public/scripts/services/votos.client.service.js
+++ public/scripts/services/votos.client.service.js
@@ -9,24 +9,28 @@
         service.votosBlancoByCanton = $resource('/api/votos/votosBlancosCanton/:codeCanton');
         service.votosBlancoByParroquia = $resource('/api/votos/votosBlancosParroquia/:codeParroquia');
         service.votosBlancoFiltro = $resource('/api/votos/votosBlancosFiltro');
+        service.votosBlancoByRegion = $resource('/api/votos/votosBlancosByRegion/:codeRegion');
 
         service.votosNulosTotal = $resource('/api/votos/votosNulos');
         service.votosNulosByProvince = $resource('/api/votos/votosNulosProvincia/:codeProvince');
         service.votosNulosByCanton = $resource('/api/votos/votosNulosCanton/:codeCanton');
         service.votosNulosByParroquia = $resource('/api/votos/votosNulosParroquia/:codeParroquia');
         service.votosNulosFiltro = $resource('/api/votos/votosNulosFiltro');
+        service.votosNulosByRegion = $resource('/api/votos/votosNulosByRegion/:codeRegion');
 
         service.votosTotales = $resource('/api/votos/votosTotales');
         service.totalVotosByProvince = $resource('/api/votos/votosTotalesProvincia/:codeProvince');
         service.totalVotosByCanton = $resource('/api/votos/votosTotalesCanton/:codeCanton');
         service.totalVotosByParroquia = $resource('/api/votos/votosTotalesParroquia/:codeParroquia');
         service.votosTotalesFiltro = $resource('/api/votos/votosTotalesFiltro');
+        service.votosTotalesByRegion = $resource('/api/votos/votosTotalesByRegion/:codeRegion');
 
         service.totalVotosLista = $resource('/api/votos/votosLista/:codeLista');
         service.totalVotosListaProvincia = $resource('/api/votos/votosListaProvince/:codeProvince/:codeLista');
         service.totalVotosListaCanton = $resource('/api/votos/votosListaCanton/:codeCanton/:codeLista');
         service.totalVotosListaParroquia = $resource('/api/votos/votosListaParroquia/:codeParroquia/:codeLista');
         service.totalVotosListaFiltro = $resource('/api/votos/totalVotosListaFiltro');
+        service.totalVotosListaByRegion = $resource('/api/votos/votosListaRegion/:codeRegion/:codeLista');
 
         service.votosByFecha = $resource('/api/votos/votosByFecha/:f1');
         service.fechasVotos = $resource('/api/votos/fecha/fechasVotos');
