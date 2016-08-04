b0f20e018857323719ffe27269d56192d2546f2e
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2016-08-03 23:08:45 -0500

    actualización

--- app/routes/junta.server.route.js
+++ app/routes/junta.server.route.js
@@ -5,11 +5,14 @@ module.exports = function (app) {
     app.route('/api/juntas/:codeRecinto/:genero')
         .get(usuario.requiresLogin, junta.getJuntasByRecintoByGenero);
 
+    app.route('/api/juntasRest/:codeRecinto/:status')
+        .get(junta.getJuntasByRecintoWithStatus);
+
     app.route('/api/juntas/:id')
         .get(usuario.requiresLogin, junta.getJunta);
 
     app.route('/api/juntasRest/')
-        .get(usuario.requiresLogin, junta.getNumeroDeVotantes);
+        .get(junta.getNumeroDeVotantes);
 
     app.route('/api/junta/numeroJuntas')
         .get(usuario.requiresLogin, junta.getNumeroJuntas);
